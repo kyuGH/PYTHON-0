58927d55c0a58c787d80e0962607c0e382bccc42
kyuGH <betcy2404@gmail.com>
2025-02-06 20:49:55 -0500

    file operaters

---
+++ 02_operators.py
@@ -0,0 +1,21 @@
+print(10 % 3) #modulo
+print(10 // 3) #div aprox
+print(2 ** 3) #potencia
+
+#concatena sin espacios(solo str)
+print("hola" + "kyu")
+print("hola" + str(5))
+print("kyu " * 5) #lo hace 5 veces
+
+#operadores comparativos
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("hola" > "kyu") #false
+
+#operadores logicos
+    #and or not
+print("operadores logicos")
+print(3 > 4 and "hola" > "python")
+print(not( 3 > 4))
